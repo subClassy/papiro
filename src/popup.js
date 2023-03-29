f999ced6712d545917a5274ef77aae08fec17c2c
Himank Goel <subclassy@outlook.com>
2023-03-29 14:09:36 -0700

    added individual clear func

--- src/popup.js
+++ src/popup.js
@@ -2,9 +2,58 @@ function clearList() {
   const readingList = document.getElementById("reading-list");
   readingList.innerHTML = "";
   chrome.storage.local.set({ 'readingList': [] });
+
+  document.getElementById("no-items-msg").style.display = "block";
+}
+
+function removeItem(event) {
+  const readingList = document.getElementById("reading-list");
+  const listItem = event.target.closest("li");
+  const itemIndex = Array.from(readingList.children).indexOf(listItem);
+
+  chrome.storage.local.get({ readingList: [] }, ({ readingList: items }) => {
+    items.splice(itemIndex, 1);
+    chrome.storage.local.set({ readingList: items });
+
+    if (items.length === 0) {
+      document.getElementById("no-items-msg").style.display = "block";
+    }
+
+    listItem.remove();
+  });
+}
+
+function createListItem(title, url) {
+  const listItem = document.createElement("li");
+  const itemInfo = document.createElement("div");
+  const itemTitle = document.createElement("span");
+  const itemDomain = document.createElement("span");
+  const itemRemove = document.createElement("span");
+
+  itemTitle.textContent = title;
+  itemTitle.classList.add("item-title");
+
+  const domainMatch = url.match(/^https?:\/\/([^/]+)/);
+  if (domainMatch) {
+    itemDomain.textContent = domainMatch[1];
+    itemDomain.classList.add("item-domain");
+  }
+
+  itemRemove.textContent = "X";
+  itemRemove.classList.add("item-remove");
+  itemRemove.addEventListener("click", removeItem);
+
+  itemInfo.appendChild(itemTitle);
+  itemInfo.appendChild(itemDomain);
+  itemInfo.appendChild(itemRemove);
+
+  listItem.appendChild(itemInfo);
+
+  return listItem;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  const readingListItem = document.getElementById("reading-list");
   const clearButton = document.getElementById("clear-button");
   clearButton.addEventListener("click", clearList);
 
@@ -12,21 +61,18 @@ document.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get(['readingList'], function(result) {
     const readingList = result.readingList || [];
 
+    if (readingList.length > 0) {
+      document.getElementById("no-items-msg").style.display = "none";
+    }
+
     // Update the popup with the reading list
-    const readingListElement = document.getElementById('reading-list');
-    readingListElement.innerHTML = '';
+    readingListItem.innerHTML = '';
 
     for (let i = 0; i < readingList.length; i++) {
         const item = readingList[i];
+        const listItem = createListItem(item.title, item.url);
 
-        const li = document.createElement('li');
-        const a = document.createElement('a');
-        a.href = item.url;
-        a.target = '_blank';
-        a.textContent = item.title;
-
-        li.appendChild(a);
-        readingListElement.appendChild(li);
+        readingListItem.appendChild(listItem);
     }
   });
 });
